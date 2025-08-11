7165c935c755e2a4be05d3771a9a6c42b43a4023
RaphaBane <raphaelaragaoparreira@gmail.com>
2025-08-11 17:17:50 -0300

    Controle de Eventos

--- config/settings.py
+++ config/settings.py
@@ -148,7 +148,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
